2ac8d84ba6a595edb73deefc6eebdff497c911dc
Pete Hahnloser <pete.hahnloser@gmail.com>
2021-06-10 09:58:49 -0500

    js file update, not live

--- assets/scripts/tipcalculator.js
+++ assets/scripts/tipcalculator.js
@@ -1,3 +1,7 @@
+let q = document.querySelector.bind(document);
+let qa = document.querySelectorAll.bind(document);
+let qi = document.getElementById.bind(document);
+
 /*
 
 inputs: bill, number of people in party, QoS
@@ -6,3 +10,20 @@ outputs: bill with tip, divided bill with tip
 
 */
 
+//let bill = Number(qi('bill').value);
+//let partySize = Number(qi('partysize').value);
+let bill = Number(document.getElementById('bill').value);
+let partySize = Number(document.getElementById('partysize').value);
+let rating = null;
+let submit = document.getElementById('submit');
+
+submit.addEventListener('click', finalBill.getTotal);
+
+
+
+
+let finalBill = {
+    getTotal: function() {
+        alert(bill);
+    }
+}
