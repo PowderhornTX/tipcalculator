ffb39279b54c4072342de8f354ade5b6029c14f5
Pete Hahnloser <pete.hahnloser@gmail.com>
2021-06-10 11:42:41 -0500

    JS test

--- assets/scripts/tipcalculator.js
+++ assets/scripts/tipcalculator.js
@@ -2,24 +2,10 @@ let q = document.querySelector.bind(document);
 let qa = document.querySelectorAll.bind(document);
 let qi = document.getElementById.bind(document);
 
-/*
-
-inputs: bill, number of people in party, QoS
-
-outputs: bill with tip, divided bill with tip
-
-tipAmount
-grandTotal
-perPerson
-
-*/
-
 let partySize = Number(qi('numberOfPeople').value);
 let ratings = ['oneStar', 'twoStar', 'threeStar', 'fourStar', 'fiveStar'];
 let submit = qi('submit');
 let qos = 0;
-
-//for (let item in )
 
 let finalBill = {
     bill: 0,
@@ -35,11 +21,6 @@ let finalBill = {
         this.bill = Number(qi('price').value);
         this.tip = (qos / 100) * this.bill;
         this.total = this.bill + this.tip;
-
-        alert(this.bill);
-        alert(this.tip);
-        alert(this.total);
-        alert(this.total / partySize);
 
         qi('tipAmount').innerHTML = this.tip;
         qi('grandTotal').innerHTML = this.total;
